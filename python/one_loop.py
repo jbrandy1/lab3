7bc696fbf21ed9983ab37c10ca0fccf902b349ea
Jacob <jbrandy1@unca.edu>
2024-02-04 14:04:44 -0500

    added set changes to one_loop.py

--- python/one_loop.py
+++ python/one_loop.py
@@ -1,4 +1,4 @@
-def one_loop(l: list) -> bool:
+def set(l: list) -> bool:
     for i in range(len(l)):
         nOE = 0
         for j in range(len(l)):
@@ -9,8 +9,8 @@ def one_loop(l: list) -> bool:
     return False    
 
 if __name__ == "__main__":
-    List1 = one_loop([1,2,3,4,5,6])
-    List2 = one_loop([23,91,54,29,23,30])
-
+    List1 = set([1,2,3,4,5,6])
+    List2 = set([23,91,54,29,23,30])
+    
     print(List1)
     print(List2)
